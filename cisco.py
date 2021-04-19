2718892ccba93a6354150469eea9d7e118bd9e73
hadifreestyler <45307346+hadifreestyler@users.noreply.github.com>
2021-04-19 14:36:08 -0500

    ssh

---
+++ cisco.py
@@ -0,0 +1,21 @@
+#sudo apt install python3-pip
+#sudo pip3 install -U netmiko
+
+#!/usr/bin/env python
+
+from netmiko import ConnectHandler
+
+nx_os = {
+
+	'device_type': 'cisco_ios',
+	'ip': 'sbx-nxos-mgmt.cisco.com',
+	'username': 'admin',
+	'password': 'Admin_1234!',
+	'port': 8181
+		}
+
+net_connect = ConnectHandler(**nx_os)
+output = net_connect.send_command('show ip int brief')
+print(output)
+
+
---
+++ ssh.py
@@ -0,0 +1,21 @@
+#sudo apt install python3-pip
+#sudo pip3 install -U netmiko
+
+#!/usr/bin/env python
+
+from netmiko import ConnectHandler
+
+nx_os = {
+
+	'device_type': 'cisco_ios',
+	'ip': '10.1.1.180',
+	'username': 'hadi',
+	'password': 'hadi',
+	'port': 22
+		}
+
+net_connect = ConnectHandler(**nx_os)
+output = net_connect.send_command('show ip int brief')
+print(output)
+
+
