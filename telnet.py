2cb3401b09973b4c3788016128c98bff5030fb83
hadifreestyler <45307346+hadifreestyler@users.noreply.github.com>
2021-04-19 14:03:51 -0500

    Update telnet.py

--- telnet.py
+++ telnet.py
@@ -1,2 +1,33 @@
-#this is a test
-#from browser
+
+import getpass
+import sys
+import telnetlib
+
+HOST = "10.1.1.180"
+
+user = input("Enter your telnet username: ")
+#user = "test"
+password = getpass.getpass()
+#password = "test"
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+
+tn.write(b"enable\n")
+tn.write(b"cisco\n")
+
+tn.write(b"conf t\n")
+tn.write(b"int loop 0\n")
+tn.write(b"ip address 1.1.1.1 255.255.255.255\n")
+
+tn.write(b"end\n")
+tn.write(b"exit\n")
+
+print (tn.read_all().decode('ascii'))
