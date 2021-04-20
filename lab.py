95b663780a989b8b65b09e4af5c14099d1385331
hadifreestyler <45307346+hadifreestyler@users.noreply.github.com>
2021-04-20 13:45:22 -0500

    Update lab.py

--- lab.py
+++ lab.py
@@ -1,9 +1,9 @@
 
 
-var = 0
+weight = input("Please Enter your weight: ")
+unit = input("please Enter the unit Kg(K) or Lbs(L): ")
 
-print (var)
-
-for n in range (1,5):
-	var =  (var) + 1
-	print (var)
+if unit.lower() == "l":
+	converted = weight / 0.45
+if unit.lower() == "k":
+	print("this is kg")
